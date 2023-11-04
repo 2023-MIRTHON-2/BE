188a356cd1f7f4119e3e4763e9ca639e86767bbb
Leespect <wewqwew153@gmail.com>
2023-11-04 23:38:08 +0900

    Refactor : 로그인 기능ë능 수정

--- users/serializers.py
+++ users/serializers.py
@@ -6,6 +6,10 @@ from allauth.account.adapter import get_adapter
 
 
 class CustomRegisterSerializer(RegisterSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'realname', 'phone', 'license', 'category', 'location']
+
     username = serializers.CharField(required=True)
     realname = serializers.CharField(required=True)
     phone = serializers.CharField(required=True)
@@ -14,35 +18,20 @@ class CustomRegisterSerializer(RegisterSerializer):
     category = serializers.ChoiceField(choices=[(tag.value, tag.value) for tag in Category])
     location = serializers.CharField(required=True)
 
-    def get_cleaned_data(self):  # 사용자가 입력한 데이터를 가져옵니다.
-        super(CustomRegisterSerializer, self).get_cleaned_data()
-
-        return {
-            'username': self.validated_data.get('username', ''),
-            'realname': self.validated_data.get('realname', ''),
-            'phone': self.validated_data.get('phone', ''),
-            'license': self.validated_data.get('license', ''),
-            # 'document': self.validated_data.get('document', ''),
-            'category': self.validated_data.get('category', ''),
-            'location': self.validated_data.get('location', ''),
-        }
-
-    def save(self, request):
-        adapter = get_adapter()
-        user = adapter.new_user(request)
-        self.cleaned_data = self.get_cleaned_data()
-        user.realname = self.cleaned_data.get('realname')
-        user.phone = self.cleaned_data.get('phone')
-        user.license = self.cleaned_data.get('license')
-        # user.profile.document = self.cleaned_data.get('document')
-        user.category = self.cleaned_data.get('category')
-        user.location = self.cleaned_data.get('location')
+    def create(self, request):
+        user = User(username=request.data['username'],
+                    realname=request.data['realname'],
+                    phone=request.data['phone'],
+                    license=request.data['license'],
+                    category=request.data['category'],
+                    location=request.data['location'],
+                    )
+        user.set_password(request.data['password'])
         user.save()
-        adapter.save_user(request, user, self)
         return user
 
 
 class CustomUserDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'realname', 'phone', 'license', 'category', 'location')
+        fields = ['username', 'realname', 'phone', 'license', 'category', 'location']
