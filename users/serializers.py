b10ac83aff515b704323a0f013d6f3a06380e59f
GOOHAESEUNG <amu6675@naver.com>
2023-11-06 03:02:40 +0900

    fix : join/login

--- users/serializers.py
+++ users/serializers.py
@@ -3,36 +3,60 @@ from rest_framework import serializers
 
 from dj_rest_auth.registration.serializers import RegisterSerializer
 from allauth.account.adapter import get_adapter
-class CustomRegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'realname', 'phone', 'license', 'category', 'location', 'is_ceo']
+class CustomRegisterSerializer(RegisterSerializer):
+    realname = serializers.CharField(max_length=255)
+    phone = serializers.CharField(max_length=50)
+    license = serializers.CharField(max_length=50)
+    category = serializers.CharField(max_length=50)
+    location = serializers.CharField(max_length=255)
+    is_ceo = serializers.BooleanField(default=True)
 
-    def create(self, request):
-        user = User.objects.create_user(username=request['username'],
-                                        realname=request['realname'],
-                                        phone=request['phone'],
-                                        license=request['license'],
-                                        category=request['category'],
-                                        location=request['location'],
-                                        is_ceo=True)
+    def get_cleaned_data(self):
+        # super 클래스의 get_cleaned_data 메소드를 호출하여 기본 데이터를 가져옵니다.
+        data = super().get_cleaned_data()
+        # 추가 필드를 cleaned_data에 추가합니다.
+        data['realname'] = self.validated_data.get('realname', '')
+        data['phone'] = self.validated_data.get('phone', '')
+        data['license'] = self.validated_data.get('license', '')
+        data['category'] = self.validated_data.get('category', '')
+        data['location'] = self.validated_data.get('location', '')
+        data['is_ceo'] = self.validated_data.get('is_ceo', True)
+        return data
 
+    def save(self, request):
+        # 부모 클래스의 save 메소드를 호출하여 사용자를 생성합니다.
+        user = super().save(request)
+        # 추가 필드를 사용자 인스턴스에 저장합니다.
+        user.realname = self.validated_data.get('realname', '')
+        user.phone = self.validated_data.get('phone', '')
+        user.license = self.validated_data.get('license', '')
+        user.category = self.validated_data.get('category', '')
+        user.location = self.validated_data.get('location', '')
+        user.is_ceo = self.validated_data.get('is_ceo', True)
+        user.save()
         return user
+class CustomRenterRegisterSerializer(RegisterSerializer):
+    realname = serializers.CharField(max_length=255)
+    phone = serializers.CharField(max_length=50)
+    is_ceo = serializers.BooleanField(default=False)
 
-class CustomRenterRegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'realname', 'phone']
+    def get_cleaned_data(self):
+        # super 클래스의 get_cleaned_data 메소드를 호출하여 기본 데이터를 가져옵니다.
+        data = super().get_cleaned_data()
+        # 추가 필드를 cleaned_data에 추가합니다.
+        data['realname'] = self.validated_data.get('realname', '')
+        data['phone'] = self.validated_data.get('phone', '')
+        data['is_ceo'] = self.validated_data.get('is_ceo', False)
+        return data
 
-    def create(self, request):
-        user = User.objects.create_user(username=request['username'],
-                                        realname=request['realname'],
-                                        phone=request['phone'],
-                                        # license=request['license'],
-                                        # category=request['category'],
-                                        # location=request['location'],
-                                        is_ceo=False)
-
+    def save(self, request):
+        # 부모 클래스의 save 메소드를 호출하여 사용자를 생성합니다.
+        user = super().save(request)
+        # 추가 필드를 사용자 인스턴스에 저장합니다.
+        user.realname = self.validated_data.get('realname', '')
+        user.phone = self.validated_data.get('phone', '')
+        user.is_ceo = self.validated_data.get('is_ceo', False)
+        user.save()
         return user
 
 class CustomUserDetailSerializer(serializers.ModelSerializer):
