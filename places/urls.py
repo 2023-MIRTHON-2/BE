61a42c4796ad2a51c58603a22d343e963387890a
Leespect <wewqwew153@gmail.com>
2023-11-05 14:49:09 +0900

    Feat : 장소 데이터 필터링 검색 기능 ãurl 추가

--- places/urls.py
+++ places/urls.py
@@ -4,7 +4,7 @@ from .views import *
 app_name = 'places'
 
 urlpatterns = [
-    # path('<str:category>/<str:location>/', PlaceListView.as_view()),
     path('', MakePlaceListView.as_view()),
+    path('<str:category>/<str:location>/', PlaceListView.as_view()),
     path('<int:post_id>/', PlaceDetailView.as_view()),
 ]
