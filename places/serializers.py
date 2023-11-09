bbb8d7a83f32615d301940d5285037f7e3da4f4e
Leespect <wewqwew153@gmail.com>
2023-11-09 23:20:19 +0900

    Style : placeImage_list -> placeImageUrl

--- places/serializers.py
+++ places/serializers.py
@@ -9,15 +9,15 @@ import time
 class PlaceImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlaceImage
-        fields = ['id', 'placeImage']
+        fields = ['placeImageUrl']
 
 
 class PlaceListSerializer(serializers.ModelSerializer):
-    placeImage = PlaceImageSerializer(read_only=True, source='placeimage_set.first')
+    placeImageUrl = PlaceImageSerializer(read_only=True, source='placeimage_set.first')
 
     class Meta:
         model = Place
-        fields = ['id', 'placeName', 'placeImage', 'business', 'location', 'article', 'cost']
+        fields = ['id', 'placeName', 'placeImageUrl', 'business', 'location', 'article', 'cost']
 
 
 class ImpossibleDateSerializer(serializers.ModelSerializer):
@@ -27,22 +27,21 @@ class ImpossibleDateSerializer(serializers.ModelSerializer):
 
 
 class PlaceSerializer(serializers.ModelSerializer):
-    placeImage_list = PlaceImageSerializer(many=True, read_only=True, source='placeimage_set')
+    placeImageUrl = PlaceImageSerializer(many=True, read_only=True, source='placeimage_set')
     impossibleDate_list = ImpossibleDateSerializer(many=True, read_only=True, source='impossibledate_set')
     ceoId = ReadOnlyField(source='ceoId.id')
 
     class Meta:
         model = Place
-        fields = ['id', 'ceoId', 'placeName', 'placeImage_list', 'licenseNum', 'lease', 'business', 'location',
+        fields = ['id', 'ceoId', 'placeName', 'placeImageUrl', 'licenseNum', 'lease', 'business', 'location',
                   'article', 'cost', 'impossibleDate_list']
 
     def create(self, validated_data):
-        images_data = self.context.get('request').FILES  # 왜 noneType이지? -> context를 설정해줘야한다. 어떻게? -> view에서 serializer를 사용할 때 context를 설정해준다.
+        images_data = self.context.get('request').FILES
         place = Place.objects.create(**validated_data)
         for image_data in images_data.getlist('placeImage'):
             PlaceImage.objects.create(placeId=place, placeImage=image_data)
         for date_data in self.context.get('request').data.getlist('impossibleDate'):
             ImpossibleDate.objects.create(placeId=place, impossibleDate=int(date_data))
-            # 예시: 1698883200
 
         return place
