c38601867d3aa7f513b85757e429df829cf863d8
Leespect <wewqwew153@gmail.com>
2023-11-10 18:17:31 +0900

    Style : urls.py 코드 컨벤션에 맞게 서식 변경

--- baloyeogi/urls.py
+++ baloyeogi/urls.py
@@ -1,17 +1,12 @@
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls import url
-
-from rest_framework import routers
-from rest_framework import permissions
-
 
 from drf_yasg.views import get_schema_view
 from rest_framework.permissions import AllowAny
 from drf_yasg import openapi
-
 
 # router = routers.DefaultRouter()
 
@@ -27,22 +22,20 @@ schema_view = get_schema_view(
     validators=['flex'],
     public=True,
     permission_classes=(AllowAny,),
-    # patterns=schema_url_patterns,
 
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('places/', include('places.urls')),
-    path('plans/', include('plans.urls')),
-    path('dj/', include('dj_rest_auth.urls')),
-    path('dj/signup/', include('dj_rest_auth.registration.urls')),
+                  path('admin/', admin.site.urls),
+                  path('users/', include('users.urls')),
+                  path('places/', include('places.urls')),
+                  path('plans/', include('plans.urls')),
+                  path('dj/', include('dj_rest_auth.urls')),
+                  path('dj/signup/', include('dj_rest_auth.registration.urls')),
 
+                  url(r'^swagger(?P<format>\.json|\.yaml)/$', schema_view.without_ui(cache_timeout=0),
+                      name='schema-json'),
+                  url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+                  url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
 
-    url(r'^swagger(?P<format>\.json|\.yaml)/$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
-
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
